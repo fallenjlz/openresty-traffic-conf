8e916bb0f7052c6065a5c91f2d781c4a8513697b
llo <iioelse@gmail.com>
2022-07-15 15:16:49 +0800

    update

--- conf.d/lua/io_test.lua
+++ conf.d/lua/io_test.lua
@@ -2,6 +2,7 @@
 local shell = require "resty.shell"
 -- local ok,stdout,_,_,_= shell.run("cd /usr/local/openresty/nginx/conf/conf.d/lua")
 -- local _,stdout2 = shell.run("pwd")
+os.execute("cd /usr/local/openresty/nginx/conf/conf.d/lua")
 local file = io.open("test.txt", "r")
 
 
