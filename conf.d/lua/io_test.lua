2fc48e34baedb32d0d9622adafa417485d0beb39
llo <iioelse@gmail.com>
2022-07-15 14:40:11 +0800

    update

--- conf.d/lua/io_test.lua
+++ conf.d/lua/io_test.lua
@@ -1,6 +1,9 @@
 local file = io.open("conf/conf.d/lua/test.txt", "r")
 
-if file == nil then ngx.say(os.execute('pwd')) end
+if file == nil then
+    ngx.say(os.execute('pwd'))
+    return
+end
 
 for line in file:lines() do ngx.say(line) end
 
