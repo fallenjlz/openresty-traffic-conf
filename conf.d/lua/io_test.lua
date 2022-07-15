24719b5517bfc310c590f6147d314c1e1f23fee5
llo <iioelse@gmail.com>
2022-07-15 15:11:51 +0800

    update

--- conf.d/lua/io_test.lua
+++ conf.d/lua/io_test.lua
@@ -1,11 +1,12 @@
 
 local shell = require "resty.shell"
-local ok,stdout,_,_,_= shell.run("pwd;cd /usr/local/openresty/nginx/conf/conf.d/lua")
+local ok,stdout,_,_,_= shell.run("cd /usr/local/openresty/nginx/conf/conf.d/lua")
+local _,stdout2 = shell.run("pwd")
 local file = io.open("test.txt", "r")
 
 
 if file == nil then
-    ngx.say(stdout)
+    ngx.say(stdout2)
     return
 end
 
