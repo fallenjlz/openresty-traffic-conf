3dcadfa8f20a0fdf557cdedd5cbe5fb7514087cf
llo <iioelse@gmail.com>
2022-07-16 20:34:27 +0800

    update

--- conf.d/lua/nginx_internal_variable.lua
+++ conf.d/lua/nginx_internal_variable.lua
@@ -1,6 +1,8 @@
-local x = tonumber(ngx.var.arg_x) or 
 local log = ngx.log
 local ERR = ngx.ERR
+
+local x = tonumber(ngx.var.arg_x) or 0
+
 
 log(ERR,"x:", x)
 local y = tonumber(ngx.var.arg_y) or 0
