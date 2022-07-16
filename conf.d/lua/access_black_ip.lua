91f8965b61cecfaf8c6d7a2abc255f5fff827920
llo <iioelse@gmail.com>
2022-07-16 20:41:03 +0800

    update

--- conf.d/lua/access_black_ip.lua
+++ conf.d/lua/access_black_ip.lua
@@ -1,4 +1,4 @@
 local black_ips = {["127.0.0.1"] = true}
 local ip = ngx.var.remote_addr
-ngx.say(ngx.ERR,"remote_addr",ip)
+ngx.log(ngx.ERR,"remote_addr",ip)
 if true == black_ips[ip] then ngx.exit(ngx.HTTP_FORBIDDEN) end
