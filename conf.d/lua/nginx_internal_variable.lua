0c3893d233add96c85690efc031052cef59891cc
llo <iioelse@gmail.com>
2022-07-16 20:12:34 +0800

    update

--- conf.d/lua/nginx_internal_variable.lua
+++ conf.d/lua/nginx_internal_variable.lua
@@ -1,5 +1,7 @@
 local x = tonumber(ngx.var.args_x) or 0 
-ngx.log(ngx.INFO,"x:" .. x)
+ngx.log(ngx.INFO,"x:", x)
+ngx.log(ngx.ERR,"x:", x)
 local y = tonumber(ngx.var.args_y) or 0 
-ngx.log(ngx.INFO,"\ny:" .. y)
+ngx.log(ngx.INFO,"y:",y)
+ngx.log(ngx.ERR,"y:",y)
 ngx.say("sum: ",x+y)
