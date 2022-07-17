093201cea357ceff15ec373ccd6849ce82e2c601
llo <iioelse@gmail.com>
2022-07-17 22:27:56 +0800

    redis pipeline reduce requests

---
+++ conf.d/lua/withoutpipeline.lua
@@ -0,0 +1,33 @@
+local redis = require "resty.redis"
+local red = redis:new()
+
+red:set_timeout(1000) -- 1 sec
+
+-- or connect to a unix domain socket file listened
+-- by a redis server:
+--     local ok, err = red:connect("unix:/path/to/redis.sock")
+
+local ok, err = red:connect("127.0.0.1", 6379)
+if not ok then
+    ngx.say("failed to connect: ", err)
+    return
+end
+
+local ok, err = red:set("cat", "Marry")
+ngx.say("set result: ", ok)
+local res, err = red:get("cat")
+ngx.say("cat: ", res)
+
+ok, err = red:set("horse", "Bob")
+ngx.say("set result: ", ok)
+res, err = red:get("horse")
+ngx.say("horse: ", res)
+
+-- put it into the connection pool of size 100,
+-- with 10 seconds max idle time
+local ok, err = red:set_keepalive(10000, 100)
+if not ok then
+    ngx.say("failed to set keepalive: ", err)
+    return
+end
+
---
+++ conf.d/lua/withpipeline.lua
@@ -0,0 +1,45 @@
+local redis = require "resty.redis"
+local red = redis:new()
+
+red:set_timeout(1000) -- 1 sec
+
+-- or connect to a unix domain socket file listened
+-- by a redis server:
+--     local ok, err = red:connect("unix:/path/to/redis.sock")
+
+local ok, err = red:connect("127.0.0.1", 6379)
+if not ok then
+    ngx.say("failed to connect: ", err)
+    return
+end
+
+red:init_pipeline()
+red:set("cat", "Marry")
+red:set("horse", "Bob")
+red:get("cat")
+red:get("horse")
+local results, err = red:commit_pipeline()
+if not results then
+    ngx.say("failed to commit the pipelined requests: ", err)
+    return
+end
+
+for i, res in ipairs(results) do
+    if type(res) == "table" then
+        if not res[1] then
+            ngx.say("failed to run command ", i, ": ", res[2])
+        else
+            -- process the table value
+        end
+    else
+        -- process the scalar value
+    end
+end
+
+-- put it into the connection pool of size 100,
+-- with 10 seconds max idle time
+local ok, err = red:set_keepalive(10000, 100)
+if not ok then
+    ngx.say("failed to set keepalive: ", err)
+    return
+end
