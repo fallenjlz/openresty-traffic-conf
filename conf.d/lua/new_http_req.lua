d2a77127bac16b1b28797497e3e64a9030612c87
llo <iioelse@gmail.com>
2022-07-17 16:41:39 +0800

    update

--- conf.d/lua/new_http_req.lua
+++ conf.d/lua/new_http_req.lua
@@ -9,7 +9,7 @@ local res,err = httpc:request_uri( "http://127.0.0.1:82/spe_md5",
                                           {method = "POST", body = args.data})
 
 if 200 ~= res.status then ngx.exit(res.status) end
-log(ERR,"\n     [args key]: ", args.key .. "\n", "      [res.body]: ",res.body .. "\n")
+log(ERR,"\n     [args key]: ", args.key .. "\n", "     [res.body]: ",res.body .. "\n")
 if args.key == res.body then
     ngx.say("valid request")
 else
