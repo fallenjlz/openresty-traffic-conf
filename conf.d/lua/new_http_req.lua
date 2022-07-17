8750a062a725673d01644b7bb7fc4646c4fb5740
llo <iioelse@gmail.com>
2022-07-17 15:51:42 +0800

    update

---
+++ conf.d/lua/new_http_req.lua
@@ -0,0 +1,15 @@
+ngx.req.read_body()
+local args, err = ngx.req.get_uri_args()
+
+local http = require "resty.http" -- ①
+local httpc = http.new()
+local res, err = httpc:request_uri( -- ②"http://127.0.0.1:82/spe_md5",
+                                          {method = "POST", body = args.data})
+
+if 200 ~= res.status then ngx.exit(res.status) end
+
+if args.key == res.body then
+    ngx.say("valid request")
+else
+    ngx.say("invalid request")
+end
