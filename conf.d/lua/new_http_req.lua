e11d456e393703f06f5de0605dbd867c44769173
llo <iioelse@gmail.com>
2022-07-17 16:37:57 +0800

    add err log

--- conf.d/lua/new_http_req.lua
+++ conf.d/lua/new_http_req.lua
@@ -1,3 +1,5 @@
+local log = ngx.log
+local ERR = ngx.ERR
 ngx.req.read_body()
 local args, err = ngx.req.get_uri_args()
 
@@ -7,7 +9,7 @@ local res,err = httpc:request_uri( "http://127.0.0.1:82/spe_md5",
                                           {method = "POST", body = args.data})
 
 if 200 ~= res.status then ngx.exit(res.status) end
-
+log(ERR,"args key: ", args.key .. "\n", "res.body: ",res.body .. "\n")
 if args.key == res.body then
     ngx.say("valid request")
 else
