32f60d83449fcad9f3198886e59f8b3b2e8dafe1
llo <iioelse@gmail.com>
2022-07-17 17:57:36 +0800

    new lib redis

---
+++ conf.d/lua/invoke_newredis.lua
@@ -0,0 +1,10 @@
+local redis = require "redis"
+local red = redis:new()
+
+local ok, err = red:set("dog", "an animal")
+if not ok then
+    ngx.say("failed to set dog: ", err)
+    return
+end
+
+ngx.say("set result: ", ok)
