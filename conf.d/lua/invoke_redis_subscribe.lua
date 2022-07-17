274d6821ea03817e1d975b05bc0000d0c5f1fa38
llo <iioelse@gmail.com>
2022-07-17 22:04:40 +0800

    update

--- conf.d/lua/invoke_redis_subscribe.lua
+++ conf.d/lua/invoke_redis_subscribe.lua
@@ -4,9 +4,7 @@ local func = red:subscribe("test")
 if not func then return nil end
 
 while true do
-    func()
-    -- if not res then return end
-    -- if res == nil or not res or err then func(false) end
-    -- ngx.say(res)
+    local res, err = func()
+    ngx.say(res)
+    if err then func(false) end
 end
-
