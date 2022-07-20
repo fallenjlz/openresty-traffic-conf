149eb375ada59e1ad543e8063564ccc108399535
llo <iioelse@gmail.com>
2022-07-20 20:41:18 +0800

    update

--- conf.d/lua/health_check.lua
+++ conf.d/lua/health_check.lua
@@ -15,8 +15,27 @@ local ok, err = hc.spawn_checker {
     concurrency = 1
 }
 
+if err == nil then
+    log(ERR, "success spawn health checker: ")
+    return
+end
 
-if ok then
+local ok, err = hc.spawn_checker {
+    shm = "healthcheck",
+    upstream = "upsrv1",
+    type = "http",
+    http_req = "GET /health HTTP/1.0\r\nHost:health.test.io\r\n\r\n",
+    interval = 3000,
+    timeout = 1000,
+    fall = 3,
+    rise = 2,
+    valid_statuses = {200,302},
+    concurrency = 1
+}
+
+
+
+if err == nil then
     log(ERR, "success spawn health checker: ")
     return
 end
