3a0423c499c17eb55d6979e279154617c469847a
llo <iioelse@gmail.com>
2022-07-20 23:41:16 +0800

    update

--- conf.d/lua/dynamic_upstream.lua
+++ conf.d/lua/dynamic_upstream.lua
@@ -6,6 +6,8 @@ local port = share_dict:get("port")
 local log = ngx.log
 local ERR = ngx.ERR
 
+log(ERR, "\nhost: ",host,"\nport: ",port)
+
 local ok,err = balancer.set_current_peer(host,port)
 if not ok then
     log(ERR, "failed to set the current peer", err)
