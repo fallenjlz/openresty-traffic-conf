22e00542b94f5dbbf3e1252efc18547144b39fdd
llo <iioelse@gmail.com>
2022-07-20 16:20:09 +0800

    update

--- conf.d/lua/health_check.lua
+++ conf.d/lua/health_check.lua
@@ -2,7 +2,7 @@ local hc = require "resty.upstream.healthcheck"
 local log = ngx.log
 local ERR = ngx.ERR
 
-local ok,err = hc.spawn_checker {
+local ok, err = hc.spawn_checker {
     upstream = "upsrv",
     type = "http",
     http_req = "GET /status HTTP/1.0\r\nHost:srv2.test.io\r\n\r\n",
@@ -10,11 +10,11 @@ local ok,err = hc.spawn_checker {
     timeout = 1000,
     fall = 3,
     rise = 2,
-    valid_statses = {2000,302},
+    valid_statses = {2000, 302},
     concurrency = 10
 }
 
 if not ok then
-    log(ERR,"faild to spawn health checker: ",error)
+    log(ERR, "faild to spawn health checker: ", error)
     return
 end
