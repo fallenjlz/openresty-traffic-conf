483f2ffad84628123fc24a181f1dfa84a4d5496d
llo <iioelse@gmail.com>
2022-07-21 11:27:22 +0800

    update

--- conf.d/lua/set_upserver.lua
+++ conf.d/lua/set_upserver.lua
@@ -1,3 +1,13 @@
 local shared_dict = ngx.shared.healthcheck
-shared_dict:set("host",ngx.var.arg_host)
-shared_dict:set("port",ngx.var.arg_port)
+
+ngx.req.read_body()
+local arg = ngx.req.get_post_args()
+
+--通过post请求体设置
+shared_dict:set("host",arg["host"])
+shared_dict:set("port",arg["port"])
+
+
+-- 通过查询参数设置
+-- shared_dict:set("host",ngx.var.arg_host)
+-- shared_dict:set("port",ngx.var.arg_port)
